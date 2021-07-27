4cbc5b46ddc5882b98da895e0996845cfb8e9b32
Kevin <kevindkevdan@gmail.com>
2021-07-27 10:21:40 -0400

    error handling

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const Post = require('./models/posts')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
+const AppError = require('./utilities/AppError')
+const catchAsync = require('./utilities/catchAsync')
+const Joi = require('joi')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +29,18 @@ mongoose.connect('mongodb://localhost:27017/showApp', { useNewUrlParser: true, u
 let viewMode = "dark";
 const author = "k6daniel";
 
+//Error checking middleware using JOI, every post needs a title to be created
+const validatePost = (req, res, next) => {
+    const postSchema = Joi.string().required()
+    const { error } = postSchema.validate(req.body.title)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new AppError(`A Title is required for a Post, ${msg}`, 400)
+    }
+    else next()
+}
+
+
 app.get('/', (req, res) => {
     res.render('home', { viewMode })
 })
@@ -41,50 +56,57 @@ app.get('/posts/create', (req, res) => {
     res.render('create', { viewMode });
 })
 
-app.get('/posts', async (req, res) => {
+app.get('/posts', catchAsync(async (req, res) => {
     const posts = await Post.find()
     const length = posts.length - 1
     res.render('posts', { posts, viewMode, length })
-})
+}))
 
 
-app.get('/posts/:id', async (req, res) => {
+app.get('/posts/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const currentPost = await Post.findById(id)
     res.render('show', { viewMode, post: currentPost })
-})
+}))
 
 
-app.get('/posts/:id/edit', async (req, res) => {
+app.get('/posts/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params;
     const currentPost = await Post.findById(id)
     res.render('edit', { viewMode, post: currentPost })
-})
+}))
 
-app.post('/posts', async (req, res) => {
-    const { title, text, image } = req.body
+app.post('/posts', validatePost, catchAsync(async (req, res) => {
+    const { title, text, image } = req.body;
+
     const newPost = new Post({ author, title, text, image })
     await newPost.save()
     res.redirect('/posts')
-})
+}))
 
-app.patch('/posts/:id', async (req, res) => {
+app.patch('/posts/:id', validatePost, catchAsync(async (req, res) => {
     const { id } = req.params;
     const { title, text, image } = req.body;
     const currentPost = await Post.findByIdAndUpdate(id, { title: title, text: text, image: image }, { runValidators: true, useFindAndModify: false })
     await currentPost.save()
     res.redirect('/posts')
-})
+}))
 
-app.delete('/posts/:id', async (req, res) => {
+app.delete('/posts/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Post.findByIdAndDelete(id, { useFindAndModify: false })
     res.redirect('/posts')
+}))
+
+app.all('*', (req, res, next) => {
+    next(new AppError('Sorry Page Not Found', 404))
 })
 
-// app.use((err, req, res, next) => {
-
-// })
+app.use((err, req, res, next) => {
+    if (!err.message) err.message = 'Something went wrong';
+    if (!err.status) err.status = 500;
+    res.status(err.status).render('error', { err, viewMode })
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
