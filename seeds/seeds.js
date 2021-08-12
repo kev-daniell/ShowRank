436d393f8e655431c387fdaf681c1ae522d84b09
Kevin <kevindkevdan@gmail.com>
2021-08-12 12:47:41 -0400

    form update

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -11,18 +11,18 @@ mongoose.connect('mongodb://localhost:27017/showApp', { useNewUrlParser: true, u
 
 const seedPosts = [
     {
-        author: '6114515d6d69a9fa796a6f02',
+        author: '611482bde319997a000202bd',
         title: 'AMC TO THE MOON',
         text: 'amc and gme r gonna go to the moon, diamond hands only baby',
     },
     {
-        author: '6114515d6d69a9fa796a6f02',
+        author: '611482bde319997a000202bd',
         title: 'What is life',
         text: 'not sure what life is anymore',
         image: 'https://i.imgflip.com/2hrn59.jpg',
     },
     {
-        author: '6114515d6d69a9fa796a6f02',
+        author: '611482bde319997a000202bd',
         title: 'Mai bad as hell',
         text: 'felt the need to remind everyone that mai is bad as hell',
         image: 'https://i.pinimg.com/originals/84/58/61/84586164248f9862b4117b2c2ab540aa.jpg'
