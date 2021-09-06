9c5521938699582ed1d6b6ed765e815bd038a7ca
Kevin <kevindkevdan@gmail.com>
2021-09-06 10:39:52 -0400

    profile update

--- models/user.js
+++ models/user.js
@@ -25,7 +25,8 @@ const userSchema = new Schema({
             type: Schema.Types.ObjectId,
             ref: 'Comment'
         }
-    ]
+    ],
+    createDate: String,
 })
 
 userSchema.plugin(passportLocalMongoose)
