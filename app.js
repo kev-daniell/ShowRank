6696e69dfd79c09126a646851a6408a15b3d71ad
Kevin <kevindkevdan@gmail.com>
2021-12-29 15:43:30 -0500

    helmet implementation

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const flash = require('connect-flash');
 const passport = require('passport')
 const localStrat = require('passport-local')
 const MongoSanitize = require('express-mongo-sanitize');
-
+const helmet = require('helmet')
 
 const AppError = require('./utilities/AppError');
 const postRoutes = require('./routes/posts');
@@ -45,6 +45,54 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(flash());
 app.use(MongoSanitize());
+app.use(helmet())
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/showappimageapi/",
+                'https://i.pinimg.com/originals/84/58/61/84586164248f9862b4117b2c2ab540aa.jpg',
+                'https://i.imgflip.com/2hrn59.jpg',
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 
 app.use(passport.initialize())
 app.use(passport.session())
